e264a7d0f4158a891d329cac9e2d98e587e0ca93
0nikk <110481533+0nikk@users.noreply.github.com>
2024-09-13 16:25:43 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -24,28 +24,28 @@ from service.models import Product, Category
 from service.common import status  # HTTP Status Codes
 from . import app
 
-
 ######################################################################
 # H E A L T H   C H E C K
 ######################################################################
+
 @app.route("/health")
 def healthcheck():
     """Let them know our heart is still beating"""
     return jsonify(status=200, message="OK"), status.HTTP_200_OK
 
-
 ######################################################################
 # H O M E   P A G E
 ######################################################################
+
 @app.route("/")
 def index():
     """Base URL for our service"""
     return app.send_static_file("index.html")
 
-
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
 def check_content_type(content_type):
     """Checks that the media type is correct"""
     if "Content-Type" not in request.headers:
@@ -63,11 +63,11 @@ def check_content_type(content_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {content_type}",
     )
-
-
+    
 ######################################################################
 # C R E A T E   A   N E W   P R O D U C T
 ######################################################################
+
 @app.route("/products", methods=["POST"])
 def create_products():
     """
@@ -85,12 +85,9 @@ def create_products():
     app.logger.info("Product with new id [%s] saved!", product.id)
 
     message = product.serialize()
-
-    #
-    # Uncomment this line of code once you implement READ A PRODUCT
-    #
-    # location_url = url_for("get_products", product_id=product.id, _external=True)
-    location_url = "/"  # delete once READ is implemented
+    
+    location_url = url_for("get_products", product_id=product.id, _external=True)
+    
     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
 
 
@@ -98,9 +95,6 @@ def create_products():
 # L I S T   A L L   P R O D U C T S
 ######################################################################
 
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
 @app.route("/products", methods=["GET"])
 def list_products():
     """Returns a list of Products"""
@@ -132,6 +126,7 @@ def list_products():
 ######################################################################
 # READ A PRODUCT
 ######################################################################
+
 @app.route("/products/<int:product_id>", methods=["GET"])
 def get_products(product_id):
     """
@@ -152,9 +147,6 @@ def get_products(product_id):
 # U P D A T E   A   P R O D U C T
 ######################################################################
 
-#
-# PLACE YOUR CODE TO UPDATE A PRODUCT HERE
-#
 @app.route("/products/<int:product_id>", methods=["PUT"])
 def update_products(product_id):
     """
@@ -174,10 +166,6 @@ def update_products(product_id):
 # D E L E T E   A   P R O D U C T
 ######################################################################
 
-
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
 @app.route("/products/<int:product_id>", methods=["DELETE"])
 def delete_products(product_id):
     """
